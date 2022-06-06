f3a8a1a0461d886f95bea1cd5fbd9f1b0c955a6d
David Gonzalez <DavidGonzalez2001-9@hotmail.com>
2022-06-06 11:10:26 +0200

    Teleport particles

---
+++ Yebba Yennebiebe/Assets/Scripts/TeleportBehaviour.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeleportBehaviour : MonoBehaviour
+{
+    public GameObject player;
+    ParticleSystem particles;
+    // Start is called before the first frame update
+    void Start()
+    {
+        particles = GetComponent<ParticleSystem>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if ((player.transform.position - particles.transform.position).magnitude <= 2)
+        {
+            //particles.Play();
+            particles.Clear();
+        }
+        else if(!particles.isPlaying)
+        {
+            particles.Play();
+        }
+    }
+}
